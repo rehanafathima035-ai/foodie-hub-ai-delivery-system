d0697c642fa5dc5126a6342bd6c59716e0abcd33
Rehana Fathima <rehanafathima035@gmail.com>
2026-08-04 23:05:11 +0530

    Prepare Flask backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -76,4 +76,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5000)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- backend/config.py
+++ backend/config.py
@@ -1,22 +1,31 @@
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 from urllib.parse import quote_plus
+load_dotenv()
+
+print("DB HOST:", os.environ.get("DB_HOST"))
+print("DB USER:", os.environ.get("DB_USER"))
+print("DB NAME:", os.environ.get("DB_NAME"))
+
 
 class Config:
     # Basic Flask Config
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'foodiehub_secret_key_12345')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = True
     
     # Database Config
     # If using local MySQL, replace with your credentials: mysql+pymysql://user:password@localhost/db_name
-    DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'Rehana@366') # Default usually empty for XAMPP/WAMP
-    ENCODED_PASSWORD = quote_plus(DB_PASSWORD)
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD','') # Default usually empty for XAMPP/WAMP
+    ENCODED_PASSWORD = quote_plus(str(DB_PASSWORD))
 
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    DB_HOST = os.environ.get('DB_HOST')
     DB_NAME = os.environ.get('DB_NAME', 'food_delivery_db')
     
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{ENCODED_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    DB_PORT = os.environ.get('DB_PORT', '4000')
+
+    SQLALCHEMY_DATABASE_URI = (f"mysql+pymysql://{DB_USER}:{ENCODED_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}""?ssl_verify_cert=true")    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Session Config
